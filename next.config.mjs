02e150f4b26cabcc566e80bd823d0a91c2c0a28a
thecoder-001 <ayush06feb@gmail.com>
2025-06-22 16:56:35 +0530

    Revert "feat: use cloudflare images for optimization"
    This reverts commit 46cabeaf2a2c0197d25a8e53806e247ecfff27cf.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    loader: "custom",
-    loaderFile: "./image-loader.ts",
-  },
-
-};
+const nextConfig = {};
 
 export default nextConfig;
